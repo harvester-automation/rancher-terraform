d6d94cef8e884d351710dd814b0da28f06631e99
Kim-Yukyung <yukyung116@gmail.com>
2026-03-13 09:14:06 +0900

    feat: support multi-node cluster configuration

--- main.tf
+++ main.tf
@@ -1,28 +1,24 @@
-# Machine configuration for RKE2 nodes using Harvester node driver
-resource "rancher2_machine_config_v2" "rke2-machine" {
-  generate_name = "rke2-machine"
+# Machine configuration for control plane nodes
+resource "rancher2_machine_config_v2" "control-plane" {
+  generate_name = "control-plane"
   harvester_config {
-    vm_namespace = "vm-ns"
-    cpu_count    = "4"
-    memory_size  = "8"
+    vm_namespace = var.vm_namespace
+    cpu_count    = var.control_plane_vm_spec.cpu_count
+    memory_size  = var.control_plane_vm_spec.memory_size
 
-    disk_info = <<-EOF
-    {
-      "disks": [{
-        "imageName": "vm-ns/image-tth9v",
-        "size": 100,
-        "bootOrder": 1
+    disk_info = jsonencode({
+      disks = [{
+        imageName = var.vm_image_name
+        size      = var.control_plane_vm_spec.disk_size
+        bootOrder = 1
       }]
-    }
-    EOF
+    })
 
-    network_info = <<-EOF
-    {
-      "interfaces": [{
-        "networkName": "vm-ns/service-vm-vlan"
+    network_info = jsonencode({
+      interfaces = [{
+        networkName = var.vm_network_name
       }]
-    }
-    EOF
+    })
 
     ssh_user = "ubuntu"
 
@@ -39,8 +35,62 @@ resource "rancher2_machine_config_v2" "rke2-machine" {
       - systemctl enable --now qemu-guest-agent
     EOF
 
-    # Use DHCP4 for internal DNS resolution (required for sslip.io)
-    # Disable DHCP routes to avoid duplicate default routes
+    network_data = <<-EOF
+    version: 2
+    ethernets:
+      enp1s0:
+        dhcp4: true
+        dhcp4-overrides:
+          use-routes: false
+        routes:
+          - to: default
+            via: 10.161.96.1
+        nameservers:
+          addresses:
+            - 8.8.8.8
+            - 8.8.4.4
+    EOF
+  }
+}
+
+# Machine configuration for worker nodes
+resource "rancher2_machine_config_v2" "worker" {
+  count          = var.worker_count > 0 ? 1 : 0
+  generate_name  = "worker"
+  harvester_config {
+    vm_namespace = var.vm_namespace
+    cpu_count    = var.worker_vm_spec.cpu_count
+    memory_size  = var.worker_vm_spec.memory_size
+
+    disk_info = jsonencode({
+      disks = [{
+        imageName = var.vm_image_name
+        size      = var.worker_vm_spec.disk_size
+        bootOrder = 1
+      }]
+    })
+
+    network_info = jsonencode({
+      interfaces = [{
+        networkName = var.vm_network_name
+      }]
+    })
+
+    ssh_user = "ubuntu"
+
+    user_data = <<-EOF
+    #cloud-config
+    password: ubuntu
+    chpasswd:
+      expire: false
+    ssh_pwauth: true
+    package_update: true
+    packages:
+      - qemu-guest-agent
+    runcmd:
+      - systemctl enable --now qemu-guest-agent
+    EOF
+
     network_data = <<-EOF
     version: 2
     ethernets:
@@ -60,28 +110,46 @@ resource "rancher2_machine_config_v2" "rke2-machine" {
 }
 
 resource "rancher2_cluster_v2" "rke2-demo" {
-  name = "rke2-terraform"
+  name = var.cluster_name
 
   # v1.34.3 fixes tigera-operator ClusterIP access issue in v1.28
   kubernetes_version = "v1.34.3+rke2r1"
 
   timeouts {
-    create = "90m"
+    create = "180m"
     update = "45m"
     delete = "45m"
   }
 
   rke_config {
+    # Control plane pool
     machine_pools {
-      name                         = "pool1"
+      name                         = "control-plane"
       cloud_credential_secret_name = local.cloud_credential_secret_name
       control_plane_role           = true
       etcd_role                    = true
-      worker_role                  = true
-      quantity                     = 1
+      worker_role                  = false
+      quantity                     = var.control_plane_count
       machine_config {
-        kind = rancher2_machine_config_v2.rke2-machine.kind
-        name = rancher2_machine_config_v2.rke2-machine.name
+        kind = rancher2_machine_config_v2.control-plane.kind
+        name = rancher2_machine_config_v2.control-plane.name
+      }
+    }
+
+    # Worker pool
+    dynamic "machine_pools" {
+      for_each = var.worker_count > 0 ? [1] : []
+      content {
+        name                         = "worker"
+        cloud_credential_secret_name = local.cloud_credential_secret_name
+        control_plane_role           = false
+        etcd_role                    = false
+        worker_role                  = true
+        quantity                     = var.worker_count
+        machine_config {
+          kind = rancher2_machine_config_v2.worker[0].kind
+          name = rancher2_machine_config_v2.worker[0].name
+        }
       }
     }
 
--- variables.tf
+++ variables.tf
@@ -27,3 +27,67 @@ variable "rancher_timeout" {
   type        = string
   default     = "30m"
 }
+
+variable "cluster_name" {
+  description = "RKE2 cluster name"
+  type        = string
+  default     = "rke2-terraform"
+}
+
+variable "control_plane_count" {
+  description = "Number of control plane nodes"
+  type        = number
+  default     = 1
+}
+
+variable "worker_count" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 0
+}
+
+variable "control_plane_vm_spec" {
+  description = "VM specification for control plane nodes"
+  type = object({
+    cpu_count   = string
+    memory_size = string
+    disk_size   = number
+  })
+  default = {
+    cpu_count   = "4"
+    memory_size = "8"
+    disk_size   = 100
+  }
+}
+
+variable "worker_vm_spec" {
+  description = "VM specification for worker nodes"
+  type = object({
+    cpu_count   = string
+    memory_size = string
+    disk_size   = number
+  })
+  default = {
+    cpu_count   = "4"
+    memory_size = "8"
+    disk_size   = 100
+  }
+}
+
+variable "vm_image_name" {
+  description = "VM image name (namespace/image-name)"
+  type        = string
+  default     = "vm-ns/image-tth9v"
+}
+
+variable "vm_network_name" {
+  description = "VM network name (namespace/network-name)"
+  type        = string
+  default     = "vm-ns/service-vm-vlan"
+}
+
+variable "vm_namespace" {
+  description = "VM namespace"
+  type        = string
+  default     = "vm-ns"
+}
